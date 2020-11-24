e08dd6e88aab43c69df2e1a0d57e53eb2c82e065
valentinalenis <32966903+valentinalenis@users.noreply.github.com>
2020-11-24 18:45:52 -0500

    some changes

--- App.py
+++ App.py
@@ -50,3 +50,9 @@ def actualizar_entrada(id):
 def eliminar_entrada(id):
     requests.delete('http://evplaneacionentrada.pythonanywhere.com/delete/'+id)
     return redirect(url_for('listarEntradas'))
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
